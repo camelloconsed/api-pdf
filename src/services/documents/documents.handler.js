8cc3cd104509efa60be2b17e2eeb5cd1ba4b8dab
Nicolas Ignacio Torres Lopez <ntorres@jumpitt.com>
2019-09-16 14:13:32 -0300

    [[ FEAT ]] Support API connection

--- src/services/documents/documents.handler.js
+++ src/services/documents/documents.handler.js
@@ -1,7 +1,9 @@
 export default ({ documentsService }) => {
   const document = {};
 
-  document.create = (docTypeID, users) => documentsService.create(docTypeID, users);
+  document.create = (docTypeID, users) => {
+    return documentsService.create(docTypeID, users);
+  };
   document.createAnnex = (docTypeID, users) => documentsService.createAnnex(docTypeID, users);
 
   return document;
